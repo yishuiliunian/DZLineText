bd4426fea0f4f546538c6c71833814cddb053174
yishuiliunian <yishuiliunian@gmail.com>
2015-12-14 14:59:27 +0800

    Initial Commit

---
+++ DZLineText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DZLineText
+//
+//  Created by baidu on 15/12/14.
+//  Copyright © 2015年 dzpqzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
